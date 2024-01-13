593b04e132168e9678004eb06d6315a1723a14a1
Leon Schreiber <leon.schreiber@tu-berlin.de>
2024-01-13 17:56:17 +0100

    Update pl-desktop.js

--- pl-desktop.js
+++ pl-desktop.js
@@ -1,4 +1,4 @@
-   define(['pipAPI', 'https://gentle-fudge-eb9646.netlify.app/xxx.js'], function (APIConstructor, iatExtension) {
+   define(['pipAPI', 'https://raw.githubusercontent.com/leonschreiber96/custom-iat/main/xxx.js`'], function (APIConstructor, iatExtension) {
       var API = new APIConstructor();
 
       return iatExtension({
